9476444657a181cd524f03af9645af7c1ed5c75a
Joan Miguel Cruz Aracena <668@CENCARCI.LOCAL>
2022-05-22 11:41:27 -0400

    Proyecto universitario PHP v0.2

--- config.php
+++ config.php
@@ -1,28 +1,21 @@
 <?php
 
-
- if (isset($_GET["Ebiografia"]))
+ if (isset($_GET["EditarBiografia"]))
  {
-   $bio = $_GET["Ebiografia"];
-   file_put_contents("biografia.txt", $bio, FILE_APPEND);
+   $bio = $_GET["EditarBiografia"];
+   file_put_contents("biografia.txt", $bio);
    header("Location: admin.php" );
     
  }
 
-
-$biografic = file_get_contents("biografia.txt", FILE_APPEND);
-$actualizar = $biografic .= $_GET["Ebiografia"];
-
-$date = date("d/m/Y");
-
 if (isset($_GET["titulo"]) && !empty($_GET["titulo"]) &&
   isset($_GET["categoria"]) && !empty($_GET["categoria"]) &&
-  isset($_GET["date"]) && !empty($_GET["date"]) &&
   isset($_GET["descripcion"]) && !empty($_GET["descripcion"])
 ) {
   $blog = file("blog.txt");
   $ultimoblog = $blog[count($blog) - 1];
-  $ultimoId = explode("|", $ultimoblog)[0];
+  $ultimoId = explode("|", $ultimoblog)[0]; 
+  $fecha = date("d/m/Y");
 
   if (!is_numeric($ultimoId)) {
     $ultimoId = 0;
@@ -32,7 +25,7 @@ if (isset($_GET["titulo"]) && !empty($_GET["titulo"]) &&
   $nuevoRegistro = "$id|";
   $nuevoRegistro .= $_GET["titulo"] . "|";
   $nuevoRegistro .= $_GET["categoria"] . "|";
-  $nuevoRegistro .= $_GET["date"] . "|";
+  $nuevoRegistro .= $fecha . "|";
   $nuevoRegistro .= $_GET["descripcion"] . "\n";
 
   //para guardar usamos file_put_contents. Pasamos la ruta como primer parametro y el contenido a guardar como segundo parametro
